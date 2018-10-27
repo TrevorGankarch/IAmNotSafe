6506e11b959544da58f566c673c06fe6195772ae
Connor Finneran <finneran.61@osu.edu>
2018-10-27 17:53:47 -0400

    working on some message stuff, not working

--- app/src/main/java/com/example/connor/iamnotsafe/MainActivity.java
+++ app/src/main/java/com/example/connor/iamnotsafe/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.connor.iamnotsafe;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.IAmNotSafe.MESSAGE";
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         String message = "TEST";
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+
+        //send message
+        String pnum = "6149156160";
+        String pmes = "Hey dude its a message cool";
+        SmsManager.getDefault().sendTextMessage(pnum, null, pmes, null, null);
+        Toast.makeText(getApplicationContext(), "Message Sent", Toast.LENGTH_LONG).show();
+//        Uri uri = Uri.parse("smsto: " + pnum);
+//        Intent intent2 = new Intent(Intent.ACTION_SENDTO, uri);
+//        intent2.putExtra("text body", pmes);
+//        startActivity(intent2);
     }
 
 }
